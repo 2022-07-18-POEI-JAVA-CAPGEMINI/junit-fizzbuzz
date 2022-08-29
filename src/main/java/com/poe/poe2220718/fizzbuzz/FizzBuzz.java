76b8ea7def0e438cdf81245465aa01c1149f9a7e
jcdominguez <jcd.pro@gmail.com>
2022-08-29 16:10:16 +0200

    first commit

---
+++ src/main/java/com/poe/poe2220718/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,12 @@
+package com.poe.poe2220718.fizzbuzz;
+
+public class FizzBuzz {
+
+    public static String play(int number){
+        
+        String reponse = "";
+      // à compléter....
+      
+      return reponse;
+    }
+}
---
+++ src/test/java/com/poe/poe20220718/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,10 @@
+package com.poe.poe20220718.fizzbuzz;
+
+
+public class FizzBuzzTest {
+
+    
+    public void monTest(){ 
+       
+    }
+}
