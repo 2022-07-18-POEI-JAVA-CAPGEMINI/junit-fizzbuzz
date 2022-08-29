b47cdaa312196e954929b53ad23425519d383c97
jcdominguez <jcd.pro@gmail.com>
2022-08-29 16:43:04 +0200

    Algo FizzBuzz + Junit

--- src/main/java/com/poe/poe2220718/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/poe/poe2220718/fizzbuzz/FizzBuzz.java
@@ -2,11 +2,21 @@ package com.poe.poe2220718.fizzbuzz;
 
 public class FizzBuzz {
 
-    public static String play(int number){
-        
-        String reponse = "";
-      // à compléter....
-      
-      return reponse;
+    public static String play(int number){ 
+
+        if(number % 15 == 0) {
+            return "FizzBuzz";
+        }
+        if(number % 3 == 0) {
+            return "Fizz";
+        }
+        if(number % 5 == 0) {
+            return "Buzz";
+        }
+
+        return ""+number;
+        // return String.valueOf(number)
+        // return Integer.toString(number)
+
     }
 }
--- src/test/java/com/poe/poe20220718/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/poe/poe20220718/fizzbuzz/FizzBuzzTest.java
@@ -1,10 +1,34 @@
 package com.poe.poe20220718.fizzbuzz;
 
+import com.poe.poe2220718.fizzbuzz.FizzBuzz;
+//import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 
 public class FizzBuzzTest {
 
+    @Test
+    public void testMultipleTrois(){ 
+        
+       assertEquals("Fizz", FizzBuzz.play(6));
+    }
     
-    public void monTest(){ 
-       
+    @Test
+    public void testMultipleCinq(){ 
+        
+       assertEquals("Buzz", FizzBuzz.play(10));
+    }
+    
+    @Test
+    public void testNombreNormal(){ 
+        
+       assertEquals("2", FizzBuzz.play(2));
+    }
+        
+    @Test
+    public void testMultipleQuinze(){ 
+        
+       assertEquals("FizzBuzz", FizzBuzz.play(15));
     }
 }
